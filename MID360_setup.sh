fba102e09351475a90d042d6a3870ba2153d91d1
Erlend Berge <33873164+MegaOdd@users.noreply.github.com>
2025-02-10 15:55:16 +0100

    Added the steps to install and build the livox_ros_driver2

--- MID360_setup.sh
+++ MID360_setup.sh
@@ -2,20 +2,21 @@
 
 cd ${ISAAC_ROS_WS}/src
 
-# Moves the livox_ros_driver2 to the /src
+# Move the livox_ros_driver2 to the /src
 mv ws_livox/src/livox_ros_driver2 .
-# Removes the livox workspace
+# Remove the livox workspace
 rm -r ws_livox
 
-
-# Change the MID360_config with our config file
+# Replace the MID360_config with our config file
 cp livox_config_launch/MID360_config.json livox_ros_driver2/config/MID360_config.json
-# Change the msg_MID360_launch file to publish pointcloud2
+# Update the msg_MID360_launch file to publish pointcloud2
 cp livox_config_launch/msg_MID360_launch.py livox_ros_driver2/launch_ROS2/msg_MID360_launch.py
 
+# Remove the package.xml file if there is one
 if [ -f livox_config_launch/package.xml ]; then
     rm livox_config_launch/package.xml
 fi
+# Create the package.xml file
 cp -f livox_config_launch/package_ROS2.xml livox_config_launch/package.xml
+# Create a launch folder with the launch files
 cp -rf livox_config_launch/launch_ROS2/ livox_config_launch/launch/
-
