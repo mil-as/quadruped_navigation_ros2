03ba136296f808126b99c16979bbe816c8385c1c
Erlend Berge <33873164+MegaOdd@users.noreply.github.com>
2025-02-13 13:41:19 +0100

    Remap /livox/lidar to /pointcloud

--- src/livox_config_launch/msg_MID360_launch.py
+++ src/livox_config_launch/msg_MID360_launch.py
@@ -38,7 +38,11 @@ def generate_launch_description():
         executable='livox_ros_driver2_node',
         name='livox_lidar_publisher',
         output='screen',
-        parameters=livox_ros2_params
+        parameters=livox_ros2_params,
+
+        remappings=[
+            (f'/livox/lidar', '/pointcloud'),
+        ]
         )
 
     return LaunchDescription([
