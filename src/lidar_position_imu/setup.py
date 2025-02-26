ecf85d39ea61f877debebd021d4ea0f509e0036b
Erlend Odd Berge <erlendob@uia.no>
2025-02-26 15:52:26 +0100

    Added a lunch file

--- src/lidar_position_imu/setup.py
+++ src/lidar_position_imu/setup.py
@@ -1,3 +1,5 @@
+import os
+from glob import glob
 from setuptools import find_packages, setup
 
 package_name = 'lidar_position_imu'
@@ -10,7 +12,9 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name), glob('launch/*.launch.py'))
     ],
+    
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='erlend',
