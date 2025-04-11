ec3704a0627ef4c88083bd63fa9e0e40e9ad3cb2
Erlend Odd Berge <erlendob@uia.no>
2025-04-11 14:39:48 +0200

    moved the replace xacro file to if statement

--- spot_setup.sh
+++ spot_setup.sh
@@ -61,13 +61,10 @@ cd ..
 if [ -d "spot_ros2" ]; then
     # Move the mesh files to spot description 
     cp -r spot_description_config/tassen spot_ros2/spot_description/spot_description/meshes
-    
     cp -r spot_description_config/LIVOX spot_ros2/spot_description/spot_description/meshes
-    
     cp -r spot_description_config/ZED spot_ros2/spot_description/spot_description/meshes
-    
     cp -r spot_description_config/tassen.urdf.xacro spot_ros2/spot_description/spot_description/urdf
+    # Replace the spot_macro file to modify it to accept tassen
+    cp spot_description_config/spot_macro.xacro spot_ros2/spot_description/spot_description/urdf/spot_macro.xacro
 fi
 
-# Replace the spot_macro file to modify it to accept tassen
-cp spot_description_config/spot_macro.xacro spot_ros2/spot_description/spot_description/urdf/spot_macro.xacro
