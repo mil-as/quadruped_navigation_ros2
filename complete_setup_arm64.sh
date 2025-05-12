f8a75cd61575f75777729a0a56d7e5cdcbda460b
Erlend Odd Berge <erlendob@uia.no>
2025-05-12 12:19:17 +0200

    Change build to an argument

--- complete_setup_arm64.sh
+++ complete_setup_arm64.sh
@@ -4,8 +4,15 @@
 ./zed_setup.sh --arm64
 sudo ./spot_setup.sh --arm64
 
-rm -r install/ build/ log/
-
-colcon build --symlink-install --cmake-args -DBUILD_TESTING=OFF -DROS_EDITION=ROS2 -DHUMBLE_ROS=humble 
-
+case "$1" in 
+    --build)
+        echo "Cleaning up previous builds..."
+        rm -r install/ build/ log/
+        
+        echo "Starting building..."
+        colcon build --symlink-install --cmake-args -DBUILD_TESTING=OFF -DROS_EDITION=ROS2 -DHUMBLE_ROS=humble 
+        ;;
+    *)
+        ;;
+esac
 source install/setup.bash
--- complete_setup_x86.sh
+++ complete_setup_x86.sh
@@ -4,12 +4,15 @@
 ./zed_setup.sh
 sudo ./spot_setup.sh
 
-rm -r install/ build/ log/
-
-colcon build --symlink-install --cmake-args -DBUILD_TESTING=OFF -DROS_EDITION=ROS2 -DHUMBLE_ROS=humble --parallel-workers 8 
-
+case "$1" in 
+    --build)
+        echo "Cleaning up previous builds..."
+        rm -r install/ build/ log/
+        
+        echo "Starting building..."
+        colcon build --symlink-install --cmake-args -DBUILD_TESTING=OFF -DROS_EDITION=ROS2 -DHUMBLE_ROS=humble 
+        ;;
+    *)
+        ;;
+esac
 source install/setup.bash
-
-sudo apt update
-sudo apt install -y ros-humble-rqt-tf-tree  #better tf viewer
-
